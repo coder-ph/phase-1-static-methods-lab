fb16ed1cb8c08ccd3cebc92075637608baf7ac28
Phelix <phelixmbani@gmail.com>
2024-10-24 23:24:59 +0300

    static methods

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase()+ string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9-' ]/g, '')
+  }
+  static titleize(sentence) {
+    const smallWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    
+    const words = sentence.split(" ");
+    
+    const titleized = words.map((word, index) => {
+      if (index === 0) {
+        return this.capitalize(word);
+      }
+      
+      if (!smallWords.includes(word.toLowerCase())) {
+        return this.capitalize(word);
+      }
+      
+      return word.toLowerCase();
+    });
+    
+    return titleized.join(" ");
+  }
 }
+
